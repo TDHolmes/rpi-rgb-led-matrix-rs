33579a7e386e605e39f4c54baff38b50e5c4617d
Tyler Holmes <tylerdanielholmes@gmail.com>
2019-05-25 13:57:51 -0700

    cooler example

--- ledmatrix-example/src/main.rs
+++ ledmatrix-example/src/main.rs
@@ -17,10 +17,37 @@ fn main() {
     let mut main_canvas = matrix.get_canvas();
 
     while 1 == 1 {
-        for i in 0..16 {
+        for x in 0..16 {
             main_canvas.clear();
-            main_canvas.draw_line(i*2, 0, 32 - i*2, 16, 0x80, 0x80, 0x80);
-            sleep(Duration::new(0, 250000000));  // 0.25s
+            main_canvas.draw_line(
+                x*2,      0,
+                32 - x*2, 16,
+                0x80, 0x80, 0x80);
+            sleep(Duration::new(0, 100000000));  // 0.1s
+        }
+        for y in 0..16 {
+            main_canvas.clear();
+            main_canvas.draw_line(
+                32, y,
+                0, 16 - y,
+                0x80, 0x80, 0x80);
+            sleep(Duration::new(0, 100000000));  // 0.1s
+        }
+        for x in 0..16 {
+            main_canvas.clear();
+            main_canvas.draw_line(
+                32 - x*2, 0,
+                x*2, 16,
+                0x80, 0x80, 0x80);
+            sleep(Duration::new(0, 100000000));  // 0.1s
+        }
+        for y in 0..16 {
+            main_canvas.clear();
+            main_canvas.draw_line(
+                0, y,
+                32, 16 - y,
+                0x80, 0x80, 0x80);
+            sleep(Duration::new(0, 100000000));  // 0.1s
         }
     }
 }
