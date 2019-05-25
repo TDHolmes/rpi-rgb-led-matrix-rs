4ba1efebb86023e689ef082e3acbfa0a4217aca4
Tyler Holmes <tylerdanielholmes@gmail.com>
2019-05-25 16:53:51 -0700

    put in invalid sentinal

--- ledmatrix/src/helper_functions.rs
+++ ledmatrix/src/helper_functions.rs
@@ -9,10 +9,10 @@ const ARGV_MAX_SIZE: usize = 128;
 
 pub fn get_c_argc_argv() -> (usize, *const*const c_char) {
     // TODO: make this argument parsing not fixed length.
-    let mut argv: [*const c_char; ARGV_MAX_SIZE] = [CString::new("").unwrap().into_raw(); ARGV_MAX_SIZE];
+    let mut argv: [*const c_char; ARGV_MAX_SIZE] = [CString::new("INVALID").unwrap().into_raw(); ARGV_MAX_SIZE];
     let mut argc = 0;
 
-    for  argument in env::args() {
+    for argument in env::args() {
         if argc >= ARGV_MAX_SIZE {
             panic!("Too many command line options!");
         }
