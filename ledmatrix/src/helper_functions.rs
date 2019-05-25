e2271341845d71ab710ad512b474fdaa66d12d6f
Tyler Holmes <tylerdanielholmes@gmail.com>
2019-05-25 16:40:19 -0700

    add size check for a reasonable panic message

--- ledmatrix/src/helper_functions.rs
+++ ledmatrix/src/helper_functions.rs
@@ -4,15 +4,19 @@ use std::env;
 use std::ffi::CString;
 use libc::c_char;
 
+const ARGV_MAX_SIZE: usize = 128;
+
 
 pub fn get_c_argc_argv() -> (i32, *const*const c_char) {
-    // Prints each argument on a separate line
-    let mut argv: [*const c_char; 128] = [CString::new("").unwrap().into_raw(); 128];
-    let mut argc = 0;
+    // TODO: make this argument parsing not fixed length.
+    let mut argv: [*const c_char; ARGV_MAX_SIZE] = [CString::new("").unwrap().into_raw(); ARGV_MAX_SIZE];
+    let mut argc: i32 = 0;
 
     for (ind, argument) in env::args().enumerate() {
         argc += 1;
-        println!("{:?}", argument);
+        if argc >= ARGV_MAX_SIZE as i32 {
+            panic!("Too many command line options!");
+        }
         argv[ind] = CString::new(argument).unwrap().into_raw();
     }
 
