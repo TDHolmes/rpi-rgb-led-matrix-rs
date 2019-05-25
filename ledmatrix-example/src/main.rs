23d02ca9b19b64b3907778a208a518d11d12a39a
Tyler Holmes <tylerdanielholmes@gmail.com>
2019-05-25 14:36:04 -0700

    try using swap on vsync capability

--- ledmatrix-example/src/main.rs
+++ ledmatrix-example/src/main.rs
@@ -15,40 +15,41 @@ fn main() {
 
     let mut matrix = Matrix::new_from_options(&options);
     let mut main_canvas = matrix.get_canvas();
+    let mut aux_canvas = matrix.create_offscreen_canvas();
 
     while 1 == 1 {
-        for x in 0..16 {
-            main_canvas.clear();
-            main_canvas.draw_line(
-                x*2,      0,
-                32 - x*2, 16,
+        for x in (0..32).step_by(2) {
+            aux_canvas.clear();
+            aux_canvas.draw_line(
+                x,      0,
+                32 - x, 16,
                 0x80, 0x80, 0x80);
-            sleep(Duration::new(0, 100000000));  // 0.1s
+            matrix.swap_canvas_on_vsync(&mut main_canvas, &mut aux_canvas);
         }
         for y in 0..16 {
-            main_canvas.clear();
-            main_canvas.draw_line(
+            aux_canvas.clear();
+            aux_canvas.draw_line(
                 32, y,
                 0, 16 - y,
                 0x80, 0x80, 0x80);
-            sleep(Duration::new(0, 100000000));  // 0.1s
+            matrix.swap_canvas_on_vsync(&mut main_canvas, &mut aux_canvas);
         }
 
-        for x in 0..16 {
-            main_canvas.clear();
-            main_canvas.draw_line(
-                32 - x*2, 16,
-                x*2, 0,
+        for x in (0..32).step_by(2) {
+            aux_canvas.clear();
+            aux_canvas.draw_line(
+                32 - x, 16,
+                x,      0,
                 0x80, 0x80, 0x80);
-            sleep(Duration::new(0, 100000000));  // 0.1s
+            matrix.swap_canvas_on_vsync(&mut main_canvas, &mut aux_canvas);
         }
         for y in 0..16 {
-            main_canvas.clear();
-            main_canvas.draw_line(
+            aux_canvas.clear();
+            aux_canvas.draw_line(
                 0, 16 - y,
                 32, y,
                 0x80, 0x80, 0x80);
-            sleep(Duration::new(0, 100000000));  // 0.1s
+            matrix.swap_canvas_on_vsync(&mut main_canvas, &mut aux_canvas);
         }
     }
 }
