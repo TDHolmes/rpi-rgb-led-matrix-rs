20c63854e9ee424273e400d58628bbcf95ec5cc9
Tyler Holmes <tylerdanielholmes@gmail.com>
2019-05-25 16:47:09 -0700

    less debug - segfault resolve

--- ledmatrix/src/helper_functions.rs
+++ ledmatrix/src/helper_functions.rs
@@ -12,12 +12,10 @@ pub fn get_c_argc_argv() -> (usize, *const*const c_char) {
     let mut argv: [*const c_char; ARGV_MAX_SIZE] = [CString::new("").unwrap().into_raw(); ARGV_MAX_SIZE];
     let mut argc = 0;
 
-    println!("args from env::args():");
     for  argument in env::args() {
         if argc >= ARGV_MAX_SIZE {
             panic!("Too many command line options!");
         }
-        println!("\t{:?}", argument);
         argv[argc] = CString::new(argument).unwrap().into_raw();
         argc += 1;
     }
